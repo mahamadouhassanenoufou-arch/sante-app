93fdfe93d190332d8a35fff27e1f3730cb108220
Mahamadou <mahamadouhassanenoufou@gmail.com>
2026-08-29 17:09:34 +0200

    Fix: chargement explicite de DATABASE_URL

--- database.py
+++ database.py
@@ -3,13 +3,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from urllib.parse import quote_plus
 
-# Base de données locale de secours
+# Base locale de secours
 password = quote_plus("Bosspapusto97@")
 LOCAL_DB_URL = f"postgresql://postgres:{password}@localhost:5432/sante_db"
 
-# Récupération de l'URL cloud de Render
+# Force la lecture de la variable Render
 DATABASE_URL = os.environ.get("DATABASE_URL", LOCAL_DB_URL)
 
+# Correction du préfixe postgres:// pour SQLAlchemy
 if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
