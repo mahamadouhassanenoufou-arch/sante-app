ad6e63171abce31ecfcd1d8285199dd2fccc9684
Mahamadou <mahamadouhassanenoufou@gmail.com>
2026-08-30 18:42:37 +0200

    Fix: Ajout du schéma UserLogin dans schemas.py

--- schemas.py
+++ schemas.py
@@ -1,6 +1,18 @@
 from pydantic import BaseModel, EmailStr
-from typing import Optional, List
-from datetime import datetime
+from typing import Optional
+
+# --- AUTHENTIFICATION ---
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    email: Optional[str] = None
+    role: Optional[str] = None
 
 # --- UTILISATEUR / MEDECIN ---
 class UserBase(BaseModel):
@@ -54,11 +66,3 @@ class RendezVousResponse(RendezVousBase):
     prescription: Optional[str] = None
     class Config:
         from_attributes = True
-        # --- AUTHENTIFICATION & TOKENS ---
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-class TokenData(BaseModel):
-    email: Optional[str] = None
-    role: Optional[str] = None
